7c852c98f4ccb427c615c81950d7b0098bb099d5
= <importz750@gmail.com>
2022-02-05 21:17:01 +0530

    Now using Queue for sync.

--- haru/mixins.py
+++ haru/mixins.py
@@ -7,7 +7,7 @@ class IOMixin:
     __slots__ = ("process_output", "futures", "disconnect_event")
 
     def __init__(self) -> None:
-        self.process_output: bytearray = bytearray()
+        self.process_output: asyncio.Queue = asyncio.Queue()
         self.futures: asyncio.Queue = asyncio.Queue()
 
         self.disconnect_event: asyncio.Event = asyncio.Event()
